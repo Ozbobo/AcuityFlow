25f482a2c3ee67a67ad94f5c57b35e7ee937e5fd
Osvaldo <osvaldo.gaming@outlook.com>
2026-04-08 23:12:27 -0400

    feat: wire up routing, provider, and bottom nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ShiftProvider } from './state/ShiftContext';
+import BottomNav from './components/BottomNav';
+import Setup from './pages/Setup';
+import Census from './pages/Census';
+import Assignments from './pages/Assignments';
+import About from './pages/About';
+
 export default function App() {
-  return <div>Charge Nurse Assigner</div>;
+  return (
+    <ShiftProvider>
+      <BrowserRouter>
+        <div className="app-shell">
+          <Routes>
+            <Route path="/" element={<Setup />} />
+            <Route path="/census" element={<Census />} />
+            <Route path="/assignments" element={<Assignments />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <BottomNav />
+        </div>
+      </BrowserRouter>
+    </ShiftProvider>
+  );
 }
